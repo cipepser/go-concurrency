4161e9ba1dc17f2756c13773198c298f41f8f208
cipepser <respepic@gmail.com>
2019-01-03 13:54:56 +0900

    add an example of drying up resource

--- chap1/main.go
+++ chap1/main.go
@@ -86,13 +86,66 @@ func livelock() {
 	peopleInHallway.Wait()
 }
 
+func resource() {
+	var wg sync.WaitGroup
+	var sharedLock sync.Mutex
+	const runtime = 1 * time.Second
+
+	greedyWorker := func() {
+		defer wg.Done()
+
+		var count int
+		for begin := time.Now(); time.Since(begin) <= runtime; {
+			sharedLock.Lock()
+			time.Sleep(3 * time.Nanosecond)
+			sharedLock.Unlock()
+			count++
+		}
+
+		fmt.Printf("Greedy worker was able to execute %v work loops\n", count)
+	}
+
+	politeWorker := func() {
+		defer wg.Done()
+
+		var count int
+		for begin := time.Now(); time.Since(begin) <= runtime; {
+			sharedLock.Lock()
+			time.Sleep(1 * time.Nanosecond)
+			sharedLock.Unlock()
+
+			sharedLock.Lock()
+			time.Sleep(1 * time.Nanosecond)
+			sharedLock.Unlock()
+
+			sharedLock.Lock()
+			time.Sleep(1 * time.Nanosecond)
+			sharedLock.Unlock()
+
+			count++
+		}
+
+		fmt.Printf("Polite worker was able to execute %v work loops\n", count)
+	}
+
+	wg.Add(2)
+	go greedyWorker()
+	go politeWorker()
+
+	wg.Wait()
+}
+
 func main() {
 	//deadlock()
 	//// fatal error: all goroutines are asleep - deadlock!
 
-	livelock()
-	//Barbara is trying to scoot: left right left right left right left right left right
-	//Barbara tosses her hands up in exasperation!
-	//Alice is trying to scoot: left right left right left right left right left right
-	//Alice tosses her hands up in exasperation!
+	//livelock()
+	////Barbara is trying to scoot: left right left right left right left right left right
+	////Barbara tosses her hands up in exasperation!
+	////Alice is trying to scoot: left right left right left right left right left right
+	////Alice tosses her hands up in exasperation!
+
+	resource()
+	//Greedy worker was able to execute 348772 work loops
+	//Polite worker was able to execute 287743 work loops
 }

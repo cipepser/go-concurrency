19faee41b006d01dab0ea9559e7b8b50ea673daf
cipepser <respepic@gmail.com>
2019-01-06 15:16:34 +0900

    add rwmutex

--- chap3/main.go
+++ chap3/main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"fmt"
+	"math"
+	"os"
 	"runtime"
 	"sync"
+	"text/tabwriter"
 	"time"
 )
 
@@ -190,6 +193,78 @@ func mutex() {
 	//Arithmetic complete.
 }
 
+func rwmutex() {
+	producer := func(wg *sync.WaitGroup, l sync.Locker) {
+		defer wg.Done()
+		for i := 0; i < 5; i++ {
+			l.Lock()
+			l.Unlock()
+			time.Sleep(1)
+		}
+	}
+
+	observer := func(wg *sync.WaitGroup, l sync.Locker) {
+		defer wg.Done()
+		l.Lock()
+		defer l.Unlock()
+	}
+
+	test := func(count int, mutex, rmMutex sync.Locker) time.Duration {
+		var wg sync.WaitGroup
+		wg.Add(count + 1)
+		beginTestTime := time.Now()
+		go producer(&wg, mutex)
+		for i := count; i > 0; i-- {
+			go observer(&wg, rmMutex)
+		}
+
+		wg.Wait()
+		return time.Since(beginTestTime)
+	}
+
+	tw := tabwriter.NewWriter(os.Stdout, 0, 1, 2, ' ', 0)
+	defer tw.Flush()
+
+	var m sync.RWMutex
+	fmt.Fprintf(tw, "Readers\tRWMutex\tMutex\n")
+	for i := 0; i < 20; i++ {
+		count := int(math.Pow(2, float64(i)))
+		fmt.Fprintf(
+			tw,
+			"%d\t%v\t%v\n",
+			count,
+			test(count, &m, m.RLocker()),
+			test(count, &m, &m),
+		)
+	}
+
+	//Readers  RWMutex       Mutex
+	//1        31.826µs      6.123µs
+	//2        35.848µs      69.869µs
+	//4        15.426µs      20.941µs
+	//8        24.27µs       39.03µs
+	//16       57.93µs       67.618µs
+	//32       76.532µs      78.665µs
+	//64       107.06µs      122.535µs
+	//128      103.991µs     248.311µs
+	//256      249.383µs     117.04µs
+	//512      207.498µs     215.452µs
+	//1024     333.15µs      510.055µs
+	//2048     806.2µs       767.582µs
+	//4096     1.188948ms    1.206972ms
+	//8192     2.786535ms    3.487278ms
+	//16384    7.557565ms    17.382744ms
+	//32768    28.781047ms   48.623299ms
+	//65536    81.844755ms   42.963367ms
+	//131072   36.88194ms    38.454013ms
+	//262144   68.751632ms   69.51097ms
+	//524288   147.760192ms  150.12134ms
+
+	// 実行するたびに、RWMutexとMutexの速さが変わるので他のアプリ落としたり、
+	// 環境を整えないと正しい結果にならなそう。
+	// それくらいで状況が変わってしまうならRWMutexを使う理由がないような気もする。
+}
+
 func main() {
 	//simpleWait()
 
@@ -203,5 +278,6 @@ func main() {
 	//waitgroup()
 	//waitgroupWithLoop()
 
-	mutex()
+	//mutex()
+	rwmutex()
 }

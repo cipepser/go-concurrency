d666d11cac0aa1d1738caaea8ea2c374c7c85f19
cipepser <respepic@gmail.com>
2019-01-06 15:56:07 +0900

    add broadcast

--- chap3/main.go
+++ chap3/main.go
@@ -309,6 +309,53 @@ func cond() {
 	//Adding to queue
 }
 
+func broadcast() {
+	type Button struct {
+		Clicked *sync.Cond
+	}
+	button := Button{
+		Clicked: sync.NewCond(&sync.Mutex{}),
+	}
+
+	subscribe := func(c *sync.Cond, fn func()) {
+		var goroutineRunning sync.WaitGroup
+		goroutineRunning.Add(1)
+		go func() {
+			goroutineRunning.Done()
+			c.L.Lock()
+			defer c.L.Unlock()
+			c.Wait()
+			fn()
+		}()
+		goroutineRunning.Wait()
+	}
+
+	var clickRegistered sync.WaitGroup
+	clickRegistered.Add(3)
+	subscribe(button.Clicked, func() {
+		fmt.Println("Maximizing windows.")
+		clickRegistered.Done()
+	})
+	subscribe(button.Clicked, func() {
+		fmt.Println("Displaying annoying dialog box!")
+		clickRegistered.Done()
+	})
+	subscribe(button.Clicked, func() {
+		fmt.Println("Mouse clicked.")
+		clickRegistered.Done()
+	})
+
+	button.Clicked.Broadcast()
+	// Signal()はruntimeが管理するgoroutineのFIFOを見て、先頭のgoroutineに伝える
+	// 今回のようにBroadcast()だと全員に伝える。
+
+	clickRegistered.Wait()
+
+	//Mouse clicked.
+	//Maximizing windows.
+	//Displaying annoying dialog box!
+}
+
 func main() {
 	//simpleWait()
 
@@ -325,5 +372,6 @@ func main() {
 	//mutex()
 	//rwmutex()
 
-	cond()
+	//cond()
+	broadcast()
 }

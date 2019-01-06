3dc717467f74a43b30f90e519f37c24275b1845a
cipepser <respepic@gmail.com>
2019-01-06 14:03:32 +0900

    add badRangeExpression

--- chap3/main.go
+++ chap3/main.go
@@ -31,8 +31,28 @@ func scope() {
 	fmt.Println(salutation) // welcome
 }
 
+func badRangeExpression() {
+	var wg sync.WaitGroup
+
+	for _, salutation := range []string{"hello", "greetings", "good day"} {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			fmt.Println(salutation)
+		}()
+	}
+
+	wg.Wait()
+
+	//good day
+	//good day
+	//good day
+}
+
 func main() {
 	//simpleWait()
 
-	scope()
+	//scope()
+
+	badRangeExpression()
 }

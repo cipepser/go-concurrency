bb6794f2a346195b6316a04dbf2bf867f4926840
cipepser <respepic@gmail.com>
2019-01-06 14:37:55 +0900

    add mutex

--- chap3/main.go
+++ chap3/main.go
@@ -140,6 +140,56 @@ func waitgroupWithLoop() {
 	//Hello from 2!
 }
 
+func mutex() {
+	var count int
+	var lock sync.Mutex
+
+	increment := func() {
+		lock.Lock()
+		defer lock.Unlock()
+		count++
+		fmt.Printf("Incrementing: %d\n", count)
+	}
+
+	decrement := func() {
+		lock.Lock()
+		defer lock.Unlock()
+		count--
+		fmt.Printf("Decrementing: %d\n", count)
+	}
+
+	var arithmetic sync.WaitGroup
+	for i := 0; i < 5; i++ {
+		arithmetic.Add(1)
+		go func() {
+			defer arithmetic.Done()
+			increment()
+		}()
+	}
+	for i := 0; i < 5; i++ {
+		arithmetic.Add(1)
+		go func() {
+			defer arithmetic.Done()
+			decrement()
+		}()
+	}
+
+	arithmetic.Wait()
+
+	fmt.Println("Arithmetic complete.")
+	//Incrementing: 1
+	//Incrementing: 2
+	//Incrementing: 3
+	//Decrementing: 2
+	//Incrementing: 3
+	//Decrementing: 2
+	//Decrementing: 1
+	//Decrementing: 0
+	//Decrementing: -1
+	//Incrementing: 0
+	//Arithmetic complete.
+}
+
 func main() {
 	//simpleWait()
 
@@ -151,5 +201,7 @@ func main() {
 	//sizeOfGoroutine()
 
 	//waitgroup()
-	waitgroupWithLoop()
+	//waitgroupWithLoop()
+
+	mutex()
 }

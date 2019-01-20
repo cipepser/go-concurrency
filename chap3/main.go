ce40743474c49e9339c7b2b8b2fcadadb61bce05
cipepser <respepic@gmail.com>
2019-01-20 13:52:49 +0900

    benchmark without pool

--- chap3/main.go
+++ chap3/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"math"
+	"net"
 	"os"
 	"runtime"
 	"sync"
@@ -392,6 +394,84 @@ func onceWithAnotherFunc() {
 
 }
 
+func pool() {
+	myPool := &sync.Pool{
+		New: func() interface{} {
+			fmt.Println("Creating new instance.")
+			return struct{}{}
+		},
+	}
+
+	myPool.Get()
+	instance := myPool.Get()
+	myPool.Put(instance)
+	myPool.Get()
+
+	//Creating new instance.
+	//Creating new instance.
+}
+
+func poolWithSmallMemory() {
+	var numCalcsCreated int
+	calcPool := &sync.Pool{
+		New: func() interface{} {
+			numCalcsCreated++
+			mem := make([]byte, 1024)
+			return &mem
+		},
+	}
+	calcPool.Put(calcPool.New())
+	calcPool.Put(calcPool.New())
+	calcPool.Put(calcPool.New())
+	calcPool.Put(calcPool.New())
+
+	const numWorkers = 1024 * 1024
+	var wg sync.WaitGroup
+	wg.Add(numWorkers)
+	for i := numWorkers; i > 0; i-- {
+		go func() {
+			defer wg.Done()
+
+			mem := calcPool.Get().(*[]byte)
+			defer calcPool.Put(mem)
+		}()
+	}
+
+	wg.Wait()
+	fmt.Printf("%d calculators were created.", numCalcsCreated) // 4 calculators were created.
+}
+
+func connectToservice() interface{} {
+	time.Sleep(1 * time.Second)
+	return struct{}{}
+}
+
+func startNetworkDaemon() *sync.WaitGroup {
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		server, err := net.Listen("tcp", "localhost:8080")
+		if err != nil {
+			log.Fatalf("cannot listen: %v", err)
+		}
+		defer server.Close()
+
+		wg.Done()
+
+		for {
+			conn, err := server.Accept()
+			if err != nil {
+				log.Printf("connot accept connection: %v", err)
+				continue
+			}
+			connectToservice()
+			fmt.Fprintf(conn, "")
+			conn.Close()
+		}
+	}()
+	return &wg
+}
+
 func main() {
 	//simpleWait()
 
@@ -412,5 +492,8 @@ func main() {
 	//broadcast()
 
 	//once()
-	onceWithAnotherFunc()
+	//onceWithAnotherFunc()
+
+	//pool()
+	//poolWithSmallMemory()
 }
--- chap3/main_test.go
+++ chap3/main_test.go
@@ -1,44 +1,71 @@
 package main
 
 import (
-	"sync"
+	"io/ioutil"
+	"net"
 	"testing"
 )
 
-func BenchmarkContextSwitch(b *testing.B) {
-	var wg sync.WaitGroup
-	begin := make(chan struct{})
-	c := make(chan struct{})
+//func BenchmarkContextSwitch(b *testing.B) {
+//	var wg sync.WaitGroup
+//	begin := make(chan struct{})
+//	c := make(chan struct{})
+//
+//	var token struct{}
+//	sender := func() {
+//		defer wg.Done()
+//		<-begin
+//		for i := 0; i < b.N; i++ {
+//			c <- token
+//		}
+//	}
+//
+//	receiver := func() {
+//		defer wg.Done()
+//		<-begin
+//		for i := 0; i < b.N; i++ {
+//			<-c
+//		}
+//	}
+//
+//	wg.Add(2)
+//	go sender()
+//	go receiver()
+//	b.StartTimer()
+//	close(begin)
+//	wg.Wait()
+//
+//	//❯ go test -bench=. -cpu=1
+//	//goos: darwin
+//	//goarch: amd64
+//	//pkg: github.com/cipepser/go-concurrency/chap3
+//	//BenchmarkContextSwitch 	10000000	       191 ns/op
+//	//PASS
+//	//ok  	github.com/cipepser/go-concurrency/chap3	2.133s
+//}
 
-	var token struct{}
-	sender := func() {
-		defer wg.Done()
-		<-begin
-		for i := 0; i < b.N; i++ {
-			c <- token
+func init() {
+	daemonStarted := startNetworkDaemon()
+	daemonStarted.Wait()
+}
+
+func BenchmarkNetworkRequest(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		conn, err := net.Dial("tcp", "localhost:8080")
+		if err != nil {
+			b.Fatalf("cannot dial host: %v", err)
 		}
+		if _, err := ioutil.ReadAll(conn); err != nil {
+			b.Fatalf("cannot read: %v", err)
+		}
+		conn.Close()
 	}
 
-	receiver := func() {
-		defer wg.Done()
-		<-begin
-		for i := 0; i < b.N; i++ {
-			<-c
-		}
-	}
-
-	wg.Add(2)
-	go sender()
-	go receiver()
-	b.StartTimer()
-	close(begin)
-	wg.Wait()
-
-	//❯ go test -bench=. -cpu=1
+	//❯ go test -benchtime=10s -bench=.
 	//goos: darwin
 	//goarch: amd64
 	//pkg: github.com/cipepser/go-concurrency/chap3
-	//BenchmarkContextSwitch 	10000000	       191 ns/op
+	//BenchmarkNetworkRequest-4   	      10	1004729582 ns/op
 	//PASS
-	//ok  	github.com/cipepser/go-concurrency/chap3	2.133s
+	//ok  	github.com/cipepser/go-concurrency/chap3	11.063s
 }

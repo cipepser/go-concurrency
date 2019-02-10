e43e2dd329144f3d3a6292c8c1aa6def198d1014
cipepser <respepic@gmail.com>
2019-02-10 20:42:07 +0900

    add adhocBinding

---
+++ chap4/main.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func adhocBinding() {
+	data := make([]int, 4)
+
+	loopData := func(handleData chan<- int) {
+		defer close(handleData)
+		for i := range data {
+			handleData <- data[i]
+		}
+	}
+
+	handleData := make(chan int)
+	go loopData(handleData)
+
+	for num := range handleData {
+		fmt.Println(num)
+	}
+	//0
+	//0
+	//0
+	//0
+}
+
+func main() {
+	adhocBinding()
+}

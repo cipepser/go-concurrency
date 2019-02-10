cd89fff6a3b47e07a0847c7b4cf50c870ed63934
cipepser <respepic@gmail.com>
2019-02-10 20:12:19 +0900

    add multiSelect

--- chap3/main.go
+++ chap3/main.go
@@ -685,6 +685,27 @@ func simpleSelect() {
 	//Unblocked 5.003069542s later.
 }
 
+func multiSelect() {
+	c1 := make(chan interface{})
+	close(c1)
+	c2 := make(chan interface{})
+	close(c2)
+
+	var c1Count, c2Count int
+	for i := 1000; i >= 0; i-- {
+		select {
+		case v := <-c1:
+			c1Count++
+		case <-c2:
+			c2Count++
+		}
+	}
+
+	fmt.Printf("c1Count: %d\nc2Count: %d\n", c1Count, c2Count)
+	//c1Count: 478
+	//c2Count: 523
+}
+
 func main() {
 	//simpleWait()
 
@@ -725,5 +746,7 @@ func main() {
 
 	//ownedChannel()
 
-	simpleSelect()
+	//simpleSelect()
+
+	multiSelect()
 }

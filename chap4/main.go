8ac88da10586c9ba0d3c3d240ae22731be096f17
cipepser <respepic@gmail.com>
2019-02-11 19:59:04 +0900

    add mutexBinding and .gitignore

--- chap4/main.go
+++ chap4/main.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"bytes"
+	"fmt"
+	"sync"
+)
 
 func adhocBinding() {
 	data := make([]int, 4)
@@ -24,6 +28,61 @@ func adhocBinding() {
 	//0
 }
 
+func lexicalBinding() {
+	chanOwner := func() <-chan int {
+		results := make(chan int, 5)
+		go func() {
+			defer close(results)
+			for i := 0; i <= 5; i++ {
+				results <- i
+			}
+		}()
+		return results
+	}
+
+	consumer := func(results <-chan int) {
+		for result := range results {
+			fmt.Printf("Received: %d\n", result)
+		}
+		fmt.Println("Done receiving!")
+	}
+
+	results := chanOwner()
+	consumer(results)
+
+	//Received: 0
+	//Received: 1
+	//Received: 2
+	//Received: 3
+	//Received: 4
+	//Received: 5
+	//Done receiving!
+}
+
+func mutexBinding() {
+	printData := func(wg *sync.WaitGroup, data []byte) {
+		defer wg.Done()
+
+		var buff bytes.Buffer
+		for _, b := range data {
+			fmt.Fprintf(&buff, "%c", b)
+		}
+		fmt.Println(buff.String())
+	}
+
+	var wg sync.WaitGroup
+	wg.Add(2)
+	data := []byte("golang")
+	go printData(&wg, data[:3])
+	go printData(&wg, data[3:])
+
+	wg.Wait()
+	//ang
+	//gol
+}
+
 func main() {
-	adhocBinding()
+	//adhocBinding()
+	//lexicalBinding()
+	mutexBinding()
 }

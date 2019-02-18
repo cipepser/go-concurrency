161257ebc25ba0a0b70d18f9b5b9bcf7a68d05a3
cipepser <respepic@gmail.com>
2019-02-18 12:36:28 +0900

    add _tee

--- chap4/main.go
+++ chap4/main.go
@@ -552,34 +552,102 @@ func repeats() {
 	//message: Iam.Iam.I...
 }
 
-func _orDone() {
-	//orDone := func(done, c <-chan interface{}) <-chan interface{} {
-	//	valStream := make(chan interface{})
-	//	go func() {
-	//		defer close(valStream)
-	//		for {
-	//			select {
-	//			case <-done:
-	//				return
-	//			case v, ok := <-c:
-	//				if !ok {
-	//					return
-	//				}
-	//				select {
-	//				case valStream <- v:
-	//				case <-done:
-	//				}
-	//
-	//			}
-	//		}
-	//	}()
-	//	return valStream
-	//}
+func orDone(done, c <-chan interface{}) <-chan interface{} {
+	valStream := make(chan interface{})
+	go func() {
+		defer close(valStream)
+		for {
+			select {
+			case <-done:
+				return
+			case v, ok := <-c:
+				if !ok {
+					return
+				}
+				select {
+				case valStream <- v:
+				case <-done:
+				}
 
-	//done := make(chan interface{})
-	//for val := range orDone(done, myChan) {
-	//	doSomething(val)
-	//}
+			}
+		}
+	}()
+	return valStream
+}
+
+func _tee() {
+	repeat := func(done <-chan interface{}, values ...interface{}) <-chan interface{} {
+		valueStream := make(chan interface{})
+		go func() {
+			defer close(valueStream)
+			for {
+				for _, v := range values {
+					select {
+					case <-done:
+						return
+					case valueStream <- v:
+					}
+				}
+			}
+		}()
+		return valueStream
+	}
+
+	take := func(
+		done <-chan interface{},
+		valueStream <-chan interface{},
+		num int,
+	) <-chan interface{} {
+		takeStream := make(chan interface{})
+		go func() {
+			defer close(takeStream)
+			for i := 0; i < num; i++ {
+				select {
+				case <-done:
+					return
+				case takeStream <- <-valueStream:
+				}
+			}
+		}()
+		return takeStream
+	}
+
+	tee := func(
+		done <-chan interface{},
+		in <-chan interface{},
+	) (_, _ <-chan interface{}) {
+		out1 := make(chan interface{})
+		out2 := make(chan interface{})
+
+		go func() {
+			defer close(out1)
+			defer close(out2)
+			for val := range orDone(done, in) {
+				var out1, out2 = out1, out2
+				for i := 0; i < 2; i++ {
+					select {
+					case out1 <- val:
+						out1 = nil
+					case out2 <- val:
+						out2 = nil
+					}
+				}
+			}
+		}()
+		return out1, out2
+	}
+
+	done := make(chan interface{})
+	defer close(done)
+
+	out1, out2 := tee(done, take(done, repeat(done, 1, 2), 4))
+	for val1 := range out1 {
+		fmt.Printf("out1: %v, out2: %v\n", val1, <-out2)
+	}
+	//out1: 1, out2: 1
+	//out1: 2, out2: 2
+	//out1: 1, out2: 1
+	//out1: 2, out2: 2
 }
 
 func main() {
@@ -605,4 +673,5 @@ func main() {
 
 	//_orDone()
 
+	_tee()
 }

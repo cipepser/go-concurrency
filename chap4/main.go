0c81ecc3ed37e445c02e39684f8b470b1e88042b
cipepser <respepic@gmail.com>
2019-03-03 11:38:21 +0900

    add introduceContext

--- chap4/main.go
+++ chap4/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"context"
 	"fmt"
 	"math/rand"
 	"net/http"
@@ -776,6 +777,84 @@ func noContext() {
 	//goodbye world!
 }
 
+func introduceContext() {
+	locale := func(ctx context.Context) (string, error) {
+		select {
+		case <-ctx.Done():
+			return "", ctx.Err()
+		case <-time.After(1 * time.Minute):
+		}
+		return "EN/US", nil
+	}
+
+	genGreeting := func(ctx context.Context) (string, error) {
+		ctx, cancel := context.WithTimeout(ctx, 1*time.Second)
+		defer cancel()
+
+		switch locale, err := locale(ctx); {
+		case err != nil:
+			return "", err
+		case locale == "EN/US":
+			return "hello", nil
+		}
+		return "", fmt.Errorf("unsupported locale")
+	}
+
+	genFarewell := func(ctx context.Context) (string, error) {
+		switch locale, err := locale(ctx); {
+		case err != nil:
+			return "", err
+		case locale == "EN/US":
+			return "goodbye", nil
+		}
+		return "", fmt.Errorf("unsupported locale")
+	}
+
+	printGreeting := func(ctx context.Context) error {
+		greeting, err := genGreeting(ctx)
+		if err != nil {
+			return err
+		}
+		fmt.Printf("%s world!\n", greeting)
+		return nil
+	}
+
+	printFarewell := func(ctx context.Context) error {
+		farewell, err := genFarewell(ctx)
+		if err != nil {
+			return err
+		}
+		fmt.Printf("%s world!\n", farewell)
+		return nil
+	}
+
+	// main
+	var wg sync.WaitGroup
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		if err := printGreeting(ctx); err != nil {
+			fmt.Printf("cannot print greeting: %v\n", err)
+			cancel()
+		}
+	}()
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		if err := printFarewell(ctx); err != nil {
+			fmt.Printf("cannot print farewell: %v\n", err)
+		}
+	}()
+
+	wg.Wait()
+	//cannot print greeting: context deadline exceeded
+	//cannot print farewell: context canceled
+}
+
 func main() {
 	//adhocBinding()
 	//lexicalBinding()
@@ -803,5 +882,6 @@ func main() {
 
 	//_bridge()
 
-	noContext()
+	//noContext()
+	introduceContext()
 }

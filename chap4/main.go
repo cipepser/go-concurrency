7058c79c69b6604e74eb30383eede7a55afa7000
cipepser <respepic@gmail.com>
2019-03-03 13:26:00 +0900

    use self defined type ctxKey to get value from context

--- chap4/main.go
+++ chap4/main.go
@@ -941,19 +941,34 @@ func deadline() {
 	// This result is returned earlier than introduceContext()'s one.
 }
 
+type ctxKey int
+
+const (
+	ctxUserID ctxKey = iota
+	ctxAuthToken
+)
+
+func UserID(c context.Context) string {
+	return c.Value(ctxUserID).(string)
+}
+
+func AuthToken(c context.Context) string {
+	return c.Value(ctxAuthToken).(string)
+}
+
 func ProcessRequest(userID, authTolen string) {
-	ctx := context.WithValue(context.Background(), "userID", userID)
-	ctx = context.WithValue(ctx, "authToken", authTolen)
+	ctx := context.WithValue(context.Background(), ctxUserID, userID)
+	ctx = context.WithValue(ctx, ctxAuthToken, authTolen)
 	HandleResponse(ctx)
 }
 
 func HandleResponse(ctx context.Context) {
 	fmt.Printf(
-		"handling response for %v (%v)",
-		ctx.Value("userID"),
-		ctx.Value("authToken"),
+		"handling response for %v (auth: %v)",
+		UserID(ctx),
+		AuthToken(ctx),
 	)
-	//handling response for jane (abc123)
+	//handling response for jane (auth: abc123)
 }
 
 func main() {
